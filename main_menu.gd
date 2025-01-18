63eb71fd09cc4766ad12f7602c3eff2e616bc1f4
Henrik Pihl <ahvenas@gmail.com>
2025-01-18 22:05:31 +0200

    Version file for iniating a build to itch.io

--- main_menu.gd
+++ main_menu.gd
@@ -1,16 +1,14 @@
 extends Node
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
+	#if OS.has_feature("HTML5"):
+	# placeholder, hide the quit button if we are in a browser 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_button_down() -> void:
 	get_tree().quit()
-	pass # Replace with function body.
