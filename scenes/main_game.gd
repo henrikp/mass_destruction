6d03ddb89168acf3674636426fc271479eca519c
Parlora <abigailtombs@gmail.com>
2025-01-20 21:09:01 +0000

    Ground scrolling fixed
    The ground works. It generates individual instances of the ground blocks so it can be used on different area types instead of just shifting the ground along artificially. The objects are deleted off-screen and it never goes above three instances

--- scenes/main_game.gd
+++ scenes/main_game.gd
@@ -3,10 +3,17 @@ extends Node
 const PLAYER_STARTING_POSITION : = Vector2i(277,446)
 const CAMERA_STARTING_POSITION := Vector2i(640,360)
 
+var forest_ground_scene: PackedScene = preload("res://scenes/forest_ground.tscn")
+var ground_types := [forest_ground_scene]
+var ground_generated : Array
+var last_ground 
+
 var speed:float
-const START_SPEED:float = 5
+const START_SPEED:float = 1
 #const MAX_SPEED:int = 25
 var screen_size : Vector2i
+
+var ground_children_debug
 
 func _ready():
 	screen_size = get_window().size
@@ -16,7 +23,8 @@ func new_game():
 	$Player.position = PLAYER_STARTING_POSITION
 	$Player.velocity = Vector2i(0,0)
 	$Camera2D.position = CAMERA_STARTING_POSITION
-	$"forest ground".position = Vector2i(0,720)
+	$"Ground/forest ground".position = Vector2i(0,720)
+	last_ground = $"Ground/forest ground"
 
 func _process(_delta):
 	speed = START_SPEED
@@ -24,6 +32,28 @@ func _process(_delta):
 	$Player.position.x += speed
 	$Camera2D.position.x += speed
 	
-	#Update ground position
-	if $Camera2D.position.x - $"forest ground".position.x > screen_size.x * 1.5 :
-		$"forest ground".position.x += screen_size.x
+	
+	if $Camera2D.position.x - last_ground.position.x > screen_size.x * 1.5 :
+		generate_floor()
+	
+	#Deletes floors that have gone off the screen
+	for ground in ground_generated:
+		if ground.position.x < ($Camera2D.position.x - screen_size.x * 3):
+			remove_scene(ground)
+	
+	#Debugging: counts the number of children in the Ground node and prints it to the log
+	ground_children_debug = get_node("Ground").get_child_count()
+	print(ground_children_debug)
+
+func generate_floor():
+	#The generates the floor 100 pixels off the right edge of the screen
+	#Modification needed to pass in the desired floor type into the function
+	var new_ground = forest_ground_scene.instantiate()
+	get_node("Ground").add_child(new_ground)
+	new_ground.position = Vector2i($Camera2D.position.x + 128, 720)
+	last_ground = new_ground
+	ground_generated.append(new_ground)
+
+func remove_scene(scene):
+	scene.queue_free()
+	ground_generated.erase(scene)
