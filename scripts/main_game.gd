ac1bc0f848de53678dfdb59be0724f10aa9a91dc
Henrik Pihl <ahvenas@gmail.com>
2025-01-31 00:32:48 +0200

    R for radiation!

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -1,55 +1,55 @@
 extends Node
 
-signal reset_game
+const PLAYER_STARTING_POSITION: Vector2i = Vector2i(181,426)
+const CAMERA_STARTING_POSITION: Vector2i = Vector2i(640,360)
 
-const PLAYER_STARTING_POSITION := Vector2i(181,426)
-const CAMERA_STARTING_POSITION := Vector2i(640,360)
+@onready var Alien: CharacterBody2D = $Player
 
 const game_over_scene: PackedScene = preload("res://scenes/game_over.tscn")
-var restart_menu = game_over_scene.instantiate()
+var restart_menu: Node = game_over_scene.instantiate()
 
 var forest_ground_scene: PackedScene = preload("res://scenes/forest_ground.tscn")
-var ground_types := [forest_ground_scene]
-var last_ground
+var ground_types : Array[PackedScene] = [forest_ground_scene]
+var last_ground: StaticBody2D
 var ground_height : int
 var forest_tree: PackedScene = preload("res://scenes/forest_tree.tscn")
 var forest_tree_1: PackedScene = preload("res://scenes/forest_tree_1.tscn")
 var forest_tree_2: PackedScene = preload("res://scenes/forest_tree_2.tscn")
-var forest_tree_types := [forest_tree,forest_tree_1,forest_tree_2]
-var last_obstacle
+var forest_tree_types: Array[PackedScene] = [forest_tree,forest_tree_1,forest_tree_2]
+var last_obstacle: Node
 
 var objects_generated_array : Array
 
-var speed:float
-const START_SPEED:float = 5
+var speed: float
+const START_SPEED: float = 4
 #const MAX_SPEED:int = 25
 var screen_size : Vector2i
 
 #var ground_children_debug
 
-func _ready():
+func _ready() -> void:
 	SignalBus.reset_game.connect(new_game)
 	screen_size = get_window().size
 	new_game()
 
-func new_game():
-	$Player.position = PLAYER_STARTING_POSITION
-	$Player/GPUParticles2D.restart()
-	$Player/GPUParticles2D.emitting = false
-	$"Player/Sprite2D".set_animation("default") # just in case?
-	$"Player/Sprite2D".show()
-	$Player.alive = true
-	$Player.velocity = Vector2i(0,0)
-	$Camera2D.position = CAMERA_STARTING_POSITION
-	$"Ground/forest ground".position = Vector2i(0,720)
+func new_game() -> void:
+	($Player as Player).position = PLAYER_STARTING_POSITION
+	($Player as Player).ParticleGen.restart()
+	($Player as Player).ParticleGen.emitting = false
+	($Player as Player).PlayerSprite.set_animation("default") # just in case?
+	($Player as Player).PlayerSprite.show()
+	($Player as Player).alive = true
+	($Player as Player).velocity = Vector2i(0,0)
+	($Camera2D as Camera2D).position = CAMERA_STARTING_POSITION
+	($"Ground/forest ground" as StaticBody2D).position = Vector2i(0,720)
 	last_ground = $"Ground/forest ground"
-	ground_height = last_ground.get_node("Sprite2D").texture.get_height()
-	$"CenterContainer/Game restart menu".hide()
-	$Player/Sprite2D.play("default")
-	$Player.reset()
+	ground_height = (last_ground.get_node("Sprite2D") as Sprite2D).texture.get_height()
+	($"CenterContainer/Game restart menu" as Node2D).hide()
+	(Alien as Player).play_animation("default")
+	(Alien as Player).reset()
 	#print_debug("Starting new game")
 
-func _process(_delta):
+func _process(_delta: float) -> void:
 	if $Player.alive == true:
 		
 		speed = START_SPEED
@@ -67,7 +67,7 @@ func _process(_delta):
 		
 		#Deletes objects that have gone off the screen
 		#The 640 is to get the centre of the camera (the camera position) and the 128 is the buffer (adjustable)
-		for object in objects_generated_array:
+		for object: Node2D in objects_generated_array:
 			if object.position.x + object.get_node("Sprite2D").texture.get_width() + 640 + 128 < $Camera2D.position.x:
 				remove_object(object)
 		
@@ -76,35 +76,34 @@ func _process(_delta):
 		#print_debug(ground_children_debug)
 	else:
 		# We are dead
-		if !$"Player/GPUParticles2D".emitting:
-			$"CenterContainer/Game restart menu".show()
-			var screen_center : Vector2 = $Camera2D.get_screen_center_position()
+		if !($Player).ParticleGen.emitting:
+			($"CenterContainer/Game restart menu" as Node2D).show()
+			var screen_center: Vector2 = ($Camera2D as Camera2D).get_screen_center_position()
 			$"CenterContainer/Game restart menu".position = Vector2(screen_center.x - 100, screen_center.y - 100)
 		
-func generate_floor():
+func generate_floor() -> void:
 	#The generates the floor 100 pixels off the right edge of the screen
 	#Modification needed to pass in the desired floor type into the function
-	var new_ground = forest_ground_scene.instantiate()
+	var new_ground: Node = forest_ground_scene.instantiate()
 	get_node("Ground").add_child(new_ground)
 	new_ground.position = Vector2i($Camera2D.position.x + 128, 720)
 	last_ground = new_ground
 	objects_generated_array.append(new_ground)
 
-func generate_obs():
+func generate_obs() -> void:
 	#Generates ground obstacles
-	var obstacle_type = forest_tree_types[randi() % forest_tree_types.size()]
-	var obs
-	obs = obstacle_type.instantiate()
+	var obstacle_type: PackedScene = forest_tree_types[randi() % forest_tree_types.size()]
+	var obs: Node = obstacle_type.instantiate()
 	#var obs_height = obs.get_node("Sprite2D").texture.get_height()
 	#var obs_scale = obs.get_node("Sprite2D").scale
-	var obs_x : int = $Camera2D.position.x + screen_size.x + 128
-	var obs_y : int = screen_size.y - ground_height + 10
+	var obs_x: int = $Camera2D.position.x + screen_size.x + 128
+	var obs_y: int = screen_size.y - ground_height + 10
 	last_obstacle = obs
-	obs.position = Vector2i(obs_x,obs_y)
+	(obs as Node2D).position = Vector2i(obs_x, obs_y)
 	get_node("Obstacles").add_child(obs)
 	objects_generated_array.append(obs)
 
-func remove_object(object):
+func remove_object(object: Node) -> void:
 	object.queue_free()
 	objects_generated_array.erase(object)
 	
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,97 +2,134 @@ extends CharacterBody2D
 
 class_name Player
 
-const PlayerTransformationSprite : Array[CompressedTexture2D] = [
-	preload("res://assets/images/player/PlayerDefault_0.png"), # flash 
-	preload("res://assets/images/player/Player_Spike_Ball.png"), # S
-	null, # R 
-	null # D
-]
+const ParticleTexture: CompressedTexture2D = preload("res://assets/images/player/32x32-white.png")
 
-const PlayerParticleTexture : Array[CompressedTexture2D] = [
+const ParticleMaterial : Array[ShaderMaterial] = [
 	null, # flash
 	null, # S
-	null, # R
-	preload("res://assets/images/player/32x32-white.png"), # D
+	preload('res://scripts/sprite_transformation_r.tres'), # R
+	preload("res://scripts/particles_explosion.tres"), # D
 	null
 ]
 
-const PlayerParticleShader : Array[Shader] = [
-	preload("res://shaders/player_flash.gdshader"), # flash
-	preload("res://shaders/transformation_s.gdshader"), # S
-	preload("res://shaders/transformation_r.gdshader"), # R
-	preload("res://shaders/sprite_explosion.gdshader"), # D
-	null
-]
-
-const PlayerMaterial : Array[ShaderMaterial] = [
-	null, # flash
-	null, # S
-	preload("res://scripts/transformation_r.tres"), # R
+const SpriteMaterial : Array[ShaderMaterial] = [
+	preload("res://scripts/sprite_flash.tres"), # flash
+	preload("res://scripts/sprite_transformation_s.tres"), # S
+	preload("res://scripts/sprite_transformation_r.tres"), # R
 	null, # D
 	null
 ]
 
+@onready var ParticleGen: GPUParticles2D = $GPUParticles2D as GPUParticles2D
+@onready var PlayerSprite: AnimatedSprite2D = $SubViewportContainer/SubViewport/Sprite2D as AnimatedSprite2D
+@onready var ViewPort: SubViewport = $SubViewportContainer/SubViewport as SubViewport
+
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
 
-# We don't switch to FORM_DEATH, only use it to get the required shader
-enum transformation { FORM_SWITCHING = -1, FORM_NORMAL = 0, FORM_SPIKES = 1, FORM_RED = 2, FORM_DEATH = 3 } 
-var current_form = transformation.FORM_NORMAL
+enum transformation { FORM_CALM = -2, FORM_SWITCHING = -1, FORM_NORMAL = 0, FORM_SPIKES = 1, FORM_RED = 2, FORM_DEATH = 3 } 
+var current_form: int = transformation.FORM_NORMAL as int
+var real_form: int = transformation.FORM_CALM as int
 var alive : bool
+
+# GPUParticles2D default
+const particles_amount : int = 8000
+const particles_emitting : bool = false
+const particles_lifetime: int = 1
+const particles_oneshot: bool = true
 
 func _on_ready() -> void:
 	pass
 
-func _process(_delta):
+func _process(_delta: float) -> void:
 	pass
-
-func set_shader(new_shader: Shader, new_sprite: bool) -> ShaderMaterial:
-	var local_material = ShaderMaterial.new()
-	local_material.shader = new_shader
-	match current_form:
-		transformation.FORM_SWITCHING:
-			pass					
-		_:
-			print_debug("Changing sprite")
-			$Sprite2D.material = PlayerMaterial[current_form]
-	local_material.set_shader_parameter("sprite", PlayerTransformationSprite[current_form])
-	local_material.shader = PlayerParticleShader[transformation.FORM_DEATH]
-	return local_material
 
 func reset() -> void:
 	current_form = transformation.FORM_NORMAL
-	$Sprite2D.material = null
+	PlayerSprite.material = null
+	reset_ParticleGen()
+	
+func reset_ParticleGen() -> void:
+	ParticleGen.amount = particles_amount
+	ParticleGen.emitting = particles_emitting
+	ParticleGen.lifetime = particles_lifetime
+	ParticleGen.one_shot = particles_oneshot
+	
+
+func play_animation(animation: String) -> void:
+	PlayerSprite.play(animation)
 
 func _input(event: InputEvent) -> void:
 	#print_debug("InputEvent triggered: ", event.as_text())	
 	if event is InputEventKey and event.pressed and current_form != transformation.FORM_SWITCHING and self.alive:
+		reset_ParticleGen()
 		if event.is_action_pressed("transform_Destruction"):
 			# Player pressed the D key - we dissolve/destruct/dissintigrate
-			# TODO: get the shader parameters from $Sprite2D before blowing up
 			self.alive = false
-			show_form()
-			$GPUParticles2D.process_material = set_shader(PlayerParticleShader[transformation.FORM_DEATH], true)		
-			$GPUParticles2D.texture = PlayerParticleTexture[transformation.FORM_DEATH]
-			$GPUParticles2D.restart()	
-			$Sprite2D.hide()
+			# TODO: get the shader parameters from $Sprite2D before blowing up (colours)
+			ParticleGen.process_material = set_custom_sprite(ParticleMaterial[transformation.FORM_DEATH], current_form)
+			ParticleGen.texture = ParticleTexture
+			current_form = transformation.FORM_DEATH
+			ParticleGen.restart()
+			PlayerSprite.hide()	
+			clear_red_effects()
 		elif event.is_action_pressed("transform_Spikes") and current_form != transformation.FORM_SPIKES:
 			# Player pressed the S key - we get spikes
 			current_form = transformation.FORM_SPIKES
 			show_form()
-			$Sprite2D.play("spikes")
-			$GPUParticles2D.process_material = set_shader(PlayerParticleShader[current_form], true)
-			$GPUParticles2D.texture = null
+			ParticleGen.process_material = null
+			ParticleGen.texture = null
+			PlayerSprite.material = SpriteMaterial[current_form]
+			PlayerSprite.set_animation("spikes")
+			clear_red_effects()
 		elif event.is_action_pressed("transform_Red") and current_form != transformation.FORM_RED:
+			# We start to glow red in our current form 
+			real_form = current_form
 			current_form = transformation.FORM_RED
-			show_form()
-			$Sprite2D.material = set_shader(PlayerParticleShader[current_form], true)
+			PlayerSprite.material = SpriteMaterial[current_form]
+
+func clear_red_effects() -> void:
+	PlayerSprite.material = null
+	real_form = transformation.FORM_CALM
+
+func set_custom_sprite(new_material: ShaderMaterial, state: int) -> ShaderMaterial:
+	print_debug(state)
+	## Override the state if we are being affected by rage
+	if (real_form != transformation.FORM_CALM):
+		state = real_form
+	## Capture the sprite picture just before we are exploding it
+	var image_shaded: Image = ViewPort.get_texture().get_image()
+	var texture_shaded: ImageTexture = ImageTexture.create_from_image(image_shaded)
+	new_material.set_shader_parameter("sprite", texture_shaded)
+	## This is currently baked in as a hardcoded option unless we need some other effects
+	ParticleGen.texture = ParticleTexture
+	return new_material
+
+func flash() -> void:
+	PlayerSprite.material = SpriteMaterial[transformation.FORM_NORMAL]
+	(PlayerSprite.material as ShaderMaterial).set_shader_parameter("flash_modifier", 1)
+	await get_tree().create_timer(0.05).timeout
+	(PlayerSprite.material as ShaderMaterial).set_shader_parameter("flash_modifier", 0)	
+
+func _on_hitbox_area_entered(_area: Area2D) -> void:
+	pass
+#	print_debug("Player hit by area: ", area.name)
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	#print_debug("Collision with ", body.name)
+	# TODO fixme in the end, ugly
+	match body.name:
+		"Forest_Tree", "forest_tree_1", "forest_tree_2":
+			body.rotate(deg_to_rad(-90.0))
 
 func show_form() -> void:
 	print_debug("Form: ", current_form)
+
+func _on_gpu_particles_2d_finished() -> void:
+	pass
+	#print_debug("Player shader emission finished")
 	
-	
-#func _physics_process(delta: float) -> void:
+	#func _physics_process(delta: float) -> void:
 		
 	## Add the gravity.
 	#if not is_on_floor():
@@ -111,27 +148,3 @@ func show_form() -> void:
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
 #
 	#move_and_slide()
-
-
-func flash():
-	$GPUParticles2D.process_material = set_shader(PlayerParticleShader[transformation.FORM_NORMAL], false)
-	$GPUParticles2D.texture = null
-	# TODO: load the correct material first
-	$Sprite2D.material.set_shader_parameter("flash_modifier", 1)
-	await get_tree().create_timer(0.05).timeout
-	$Sprite2D.material.set_shader_parameter("flash_modifier", 0)	
-
-func _on_hitbox_area_entered(area: Area2D) -> void:
-	pass
-#	print_debug("Player hit by area: ", area.name)
-
-func _on_hitbox_body_entered(body: Node2D) -> void:
-	#print_debug("Collision with ", body.name)
-	# TODO fixme in the end, ugly
-	match body.name:
-		"Forest_Tree", "forest_tree_1", "forest_tree_2":
-			flash()
-
-func _on_gpu_particles_2d_finished() -> void:
-	pass
-	#print_debug("Player shader emission finished")
